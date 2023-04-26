fab7594a154ab14158e38bbdf468bd3fd083a78a
Juan Vistro <jpmbvistro@gmail.com>
2023-04-25 23:05:41 -0400

    Add storyblok integration with blog CMS fetching

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ![social-preview-image](https://user-images.githubusercontent.com/3909046/219942674-9894853e-def8-4180-84b8-6b577dacfcaa.png)
 
+Forked from https://github.com/markteekman/accessible-astro-starter
+
 Accessible Astro Starter is a ready to use, SEO and a11y friendly blogging theme. It contains plenty of accessible components to build several page types, Tailwind CSS to help you build faster and example pages such as a dynamic Blog, 404, Markdown and MDX. This theme is designed to help you build your project faster and provide a solid base for accessibility!
 
 🚀 [Live Preview](https://accessible-astro.netlify.app/)
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,28 @@
-import { defineConfig } from 'astro/config'
-import compress from 'astro-compress'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import compress from 'astro-compress';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import netlify from "@astrojs/netlify/functions";
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), mdx(), tailwind()],
-})
+  output: 'server',
+  integrations: [compress(), mdx(), tailwind(), storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      // Add your components here
+      blogPost: 'storyblok/BlogPost',
+      blogPostList: 'storyblok/BlogPostList',
+      page: 'storyblok/Page'
+    },
+    apiOptions: {
+      // Choose your Storyblok space region
+      region: 'us' // optional,  or 'eu' (default)
+    }
+  })],
+  adapter: netlify()
+});
